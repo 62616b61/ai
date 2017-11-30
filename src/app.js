130117c9aa791c99bbc953814d63508d87dbed11
whynotcrybot <alexanderkatsero@gmail.com>
2017-11-30 19:35:58 +0200

    changed directory structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import EventEmitter from 'events'
 
 import stages from './stages'
-import Runner from './system/runner'
-import Screen from './system/screen'
+import Runner from './system/Runner'
+import Screen from './system/Screen'
 
 export default class App {
   constructor () {
